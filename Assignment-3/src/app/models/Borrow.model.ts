2cdb034536a0225ff9c7e06752158fa21915ded9
Rakib-28169-islam <rakibulislam28169@gmail.com>
2025-07-13 02:08:53 +0600

    working with vercel hosting

--- Assignment-3/src/app/models/Borrow.model.ts
+++ Assignment-3/src/app/models/Borrow.model.ts
@@ -1,6 +1,6 @@
 import { Schema, model } from "mongoose";
 import { IBorrow } from "../interface/borrow.interface";
-import { z } from "zod";
+
 
 
 const borrowSchema = new Schema<IBorrow>(
@@ -18,15 +18,7 @@ const borrowSchema = new Schema<IBorrow>(
     dueDate: {
       type: Date,
       required: [true, "Due date is required"],
-    },
-    // createdAt: {
-    //   type: Date,
-    //   default: Date.now,
-    // },
-    // updatedAt: {
-    //   type: Date,
-    //   default: Date.now,
-    // },
+    }
   },
 
   {
@@ -34,20 +26,6 @@ const borrowSchema = new Schema<IBorrow>(
     timestamps: true,
   }
 );
-
-export const createZodBorrowSchema = z.object({
-  book: z.string().regex(/^[a-f\d]{24}$/i, "Invalid Book ID format"),
-  quantity: z.number().int().min(1, "Quantity must be at least 1 or greater than 0"),
-  dueDate: z.coerce.date({ required_error: "Due Date is required" })
-  .refine(
-    (date) => date > new Date(),
-    {
-      message: "Due Date must be in the future",
-    }
-  ),
-});
-
-
 
 const BorrowModel = model<IBorrow>("Borrows", borrowSchema);
 export default BorrowModel;
